f1e2d6a762ba493a89e03024214bdf6ef83854d0
thedocbwarren <jedi.master@augmentedjs.com>
2017-12-27 17:00:23 -0800

    new build

--- src/augmented.js
+++ src/augmented.js
@@ -44,7 +44,7 @@ import isDefined from "./functions/isDefined.js";
  * @author Bob Warren
  *
  * @module Augmented
- * @version 2.0.0-alpha.7
+ * @version 2.0.0-alpha.8
  * @license Apache-2.0
  */
 const Augmented = {};
@@ -89,7 +89,7 @@ Augmented.isDefined = isDefined;
  * The standard version property
  * @constant VERSION
  */
-Augmented.VERSION = "2.0.0-alpha.7";
+Augmented.VERSION = "2.0.0-alpha.8";
 /**
  * A codename for internal use
  * @constant codename
